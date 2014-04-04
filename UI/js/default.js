4386fb24c04e3d54325cda55c7d74deba95dc895
Xun Ding <xding@comscore.com>
2014-04-04 16:23:32 -0500

    chart, and popularity table

--- UI/js/default.js
+++ UI/js/default.js
@@ -1,36 +1,86 @@
-﻿jQuery(function($) {
-    $('.easy-pie-chart.percentage').each(function() {
-        var $box = $(this).closest('.infobox');
-        var barColor = $(this).data('color') || (!$box.hasClass('infobox-dark') ? $box.css('color') : 'rgba(255,255,255,0.95)');
-        var trackColor = barColor == 'rgba(255,255,255,0.95)' ? 'rgba(255,255,255,0.25)' : '#E2E2E2';
-        var size = parseInt($(this).data('size')) || 50;
-        $(this).easyPieChart({
-            barColor: barColor,
-            trackColor: trackColor,
-            scaleColor: false,
-            lineCap: 'butt',
-            lineWidth: parseInt(size / 10),
-            animate: /msie\s*(8|7|6)/.test(navigator.userAgent.toLowerCase()) ? false : 1000,
-            size: size
+﻿jQuery(function ($) {
+   
+    /*************************** Trend chart code ***********************/
+
+    var trendChartData = [];
+
+
+    function gd(date) {
+        return new Date(date).getTime();
+    }
+
+    function parseTrendData(data) {
+        var projectTweetTrend = [], index,
+            tdata = data.data, trend;
+        for (index = 0; index < tdata.length; index++) {
+            var info = tdata[index];
+            trend = {};
+            trend.label = info.label;
+            trend.data = [];
+            for (var j = 0; j < info.data.length; j++) {
+                var tweetinfo = info.data[j];
+                trend.data.push([gd(tweetinfo.date), tweetinfo.tweets]);
+            }
+            projectTweetTrend.push(trend);
+        }
+
+        return projectTweetTrend;
+
+    }
+    var trendPlaceholder = $('#trendchart-placeholder').css({ 'min-height': '150px' });
+    function getTrendChartData() {
+        $.getJSON('test/testdata/trenddata.txt').done(function (data) {
+
+
+            drawTrendChart(trendPlaceholder, parseTrendData(data));
+            //            console.log(parseTrendData(data));
         });
-    });
-
-    $('.sparkline').each(function() {
-        var $box = $(this).closest('.infobox');
-        var barColor = !$box.hasClass('infobox-dark') ? $box.css('color') : '#FFF';
-        $(this).sparkline('html', { tagValuesAttribute: 'data-values', type: 'bar', barColor: barColor, chartRangeMin: $(this).data('min') || 0 });
-    });
+    }
 
 
-    var placeholder = $('#piechart-placeholder').css({ 'width': '90%', 'min-height': '150px' });
-    var data = [
-        { label: "social networks", data: 38.7, color: "#68BC31" },
-        { label: "search engines", data: 24.5, color: "#2091CF" },
-        { label: "ad campaigns", data: 8.2, color: "#AF4E96" },
-        { label: "direct traffic", data: 18.6, color: "#DA5430" },
-        { label: "other", data: 10, color: "#FEE074" }
-    ];
+    function drawTrendChart(placeholder, data) {
 
+        $.plot(placeholder, data, {
+            hoverable: true,
+            shadowSize: 0,
+            series: {
+                lines: { show: true },
+                points: { show: true }
+            },
+            xaxis: {
+
+            },
+            yaxis: {
+                ticks: 10
+            },
+            grid: {
+                //backgroundColor: { colors: ["#fff", "#fff"] },
+                borderWidth: 0
+                //borderColor: '#555'
+            }
+        });
+    }
+
+
+    getTrendChartData();
+    /**
+   we saved the drawing function and the data to redraw with different position later when switching to RTL mode dynamically
+   so that's not needed actually.
+   */
+    trendPlaceholder.data('chart', trendChartData);
+    trendPlaceholder.data('draw', drawTrendChart);
+    
+
+    /************ Pie chart code *******************/
+    var pieChartData = [];
+    var placeholder = $('#piechart-placeholder').css({ 'width': '70%', 'min-height': '150px' });
+
+    function getPieChartData() {
+        $.getJSON('pietweetdata.ashx').done(function (data) {
+            pieChartData = data.data;
+            drawPieChart(placeholder, data.data);
+        });
+    }
     function drawPieChart(placeholder, data, position) {
         $.plot(placeholder, data, {
             series: {
@@ -60,20 +110,22 @@
         });
     }
 
-    drawPieChart(placeholder, data);
+    getPieChartData();
 
     /**
     we saved the drawing function and the data to redraw with different position later when switching to RTL mode dynamically
     so that's not needed actually.
     */
-    placeholder.data('chart', data);
+    placeholder.data('chart', pieChartData);
     placeholder.data('draw', drawPieChart);
 
-
+    /***
+    end of pie chart
+    **/
     var $tooltip = $("<div class='tooltip top in'><div class='tooltip-inner'></div></div>").hide().appendTo('body');
     var previousPoint = null;
 
-    placeholder.on('plothover', function(event, pos, item) {
+    placeholder.on('plothover', function (event, pos, item) {
         if (item) {
             if (previousPoint != item.seriesIndex) {
                 previousPoint = item.seriesIndex;
@@ -89,100 +141,23 @@
     });
 
 
-    var d1 = [];
-    for (var i = 0; i < Math.PI * 2; i += 0.5) {
-        d1.push([i, Math.sin(i)]);
-    }
+    
 
-    var d2 = [];
-    for (var i = 0; i < Math.PI * 2; i += 0.5) {
-        d2.push([i, Math.cos(i)]);
-    }
+    //$('#recent-box [data-rel="tooltip"]').tooltip({ placement: tooltip_placement });
 
-    var d3 = [];
-    for (var i = 0; i < Math.PI * 2; i += 0.2) {
-        d3.push([i, Math.tan(i)]);
-    }
+    //function tooltip_placement(context, source) {
+    //    var $source = $(source);
+    //    var $parent = $source.closest('.tab-content');
+    //    var off1 = $parent.offset();
+    //    var w1 = $parent.width();
 
+    //    var off2 = $source.offset();
+    //    var w2 = $source.width();
 
-    var sales_charts = $('#sales-charts').css({ 'width': '100%', 'height': '220px' });
-    $.plot("#sales-charts", [
-            { label: "Domains", data: d1 },
-            { label: "Hosting", data: d2 },
-            { label: "Services", data: d3 }
-        ], {
-            hoverable: true,
-            shadowSize: 0,
-            series: {
-                lines: { show: true },
-                points: { show: true }
-            },
-            xaxis: {
-                tickLength: 0
-            },
-            yaxis: {
-                ticks: 10,
-                min: -2,
-                max: 2,
-                tickDecimals: 3
-            },
-            grid: {
-                backgroundColor: { colors: ["#fff", "#fff"] },
-                borderWidth: 1,
-                borderColor: '#555'
-            }
-        });
+    //    if (parseInt(off2.left) < parseInt(off1.left) + parseInt(w1 / 2)) return 'right';
+    //    return 'left';
+    //}
 
-
-    $('#recent-box [data-rel="tooltip"]').tooltip({ placement: tooltip_placement });
-
-    function tooltip_placement(context, source) {
-        var $source = $(source);
-        var $parent = $source.closest('.tab-content');
-        var off1 = $parent.offset();
-        var w1 = $parent.width();
-
-        var off2 = $source.offset();
-        var w2 = $source.width();
-
-        if (parseInt(off2.left) < parseInt(off1.left) + parseInt(w1 / 2)) return 'right';
-        return 'left';
-    }
-
-
-    $('.dialogs,.comments').slimScroll({
-        height: '300px'
-    });
-
-
-    //Android's default browser somehow is confused when tapping on label which will lead to dragging the task
-    //so disable dragging when clicking on label
-    var agent = navigator.userAgent.toLowerCase();
-    if ("ontouchstart" in document && /applewebkit/.test(agent) && /android/.test(agent))
-        $('#tasks').on('touchstart', function(e) {
-            var li = $(e.target).closest('#tasks li');
-            if (li.length == 0) return;
-            var label = li.find('label.inline').get(0);
-            if (label == e.target || $.contains(label, e.target)) e.stopImmediatePropagation();
-        });
-
-    $('#tasks').sortable({
-            opacity: 0.8,
-            revert: true,
-            forceHelperSize: true,
-            placeholder: 'draggable-placeholder',
-            forcePlaceholderSize: true,
-            tolerance: 'pointer',
-            stop: function(event, ui) { //just for Chrome!!!! so that dropdowns on items don't appear below other items after being moved
-                $(ui.item).css('z-index', 'auto');
-            }
-        }
-    );
-    $('#tasks').disableSelection();
-    $('#tasks input:checkbox').removeAttr('checked').on('click', function() {
-        if (this.checked) $(this).closest('li').addClass('selected');
-        else $(this).closest('li').removeClass('selected');
-    });
 
 
 });
---
+++ UI/js/projectranking.js
@@ -0,0 +1,24 @@
+﻿jQuery(function ($) {
+    var timeline = [];
+
+    function getPopularityRanking(pos) {
+        $.getJSON('test/testdata/popularityrankingdata.txt').done(function (data) {
+           
+            setData(data);
+           
+        });
+    }
+
+    function setData(data) {
+        for (var i = 0; i < data.data.length; i++) {
+            var ranking = data.data[i];
+
+            $('#projectrankingtable').dataTable().fnAddData(
+                [ranking.name, ranking.score, ranking.totaltweets, 0]
+                );
+        }
+    }
+
+    getPopularityRanking();
+
+});
