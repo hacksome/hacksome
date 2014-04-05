b7a207a29c0dd71b013f49d18ebc0fac72b52928
Xun Ding <xding@comscore.com>
2014-04-04 22:28:54 -0500

    commit mapsapi

---
+++ UI/js/mostrecenttweets.js
@@ -0,0 +1,18 @@
+﻿jQuery(function ($) {
+
+    function getMostRecentTweets() {
+        $.getJSON('twittertweets.ashx?limit=true').done(function (data) {
+            setData(data);
+        });
+    }
+
+    function setData(data) {
+        for (var i = 0; i < data.data.length; i++) {
+           // var tweets = data.[i];
+
+        }
+    }
+
+    getMostRecentTweets();
+
+});
